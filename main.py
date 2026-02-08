f36f48542af2ee7e2da73c3d63db2537975c3fe4
d0kshy <dariakurnosova.contact@gmail.com>
2026-02-08 17:54:05 +0100

    Added a function that creates baseline.txt

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import time
 
 
+MONITOR_DIR = "./test_folder"
+BASELINE_FILE = "baseline.txt"
+
+
 def calculate_hash(filepath):
     sha256_hash = hashlib.sha256
 
@@ -17,6 +21,27 @@ def calculate_hash(filepath):
         return None
     except PermissionError:
         return None
+
+
+def create_baseline():
+    print("----- CREATING NEW BASELINE -----")
+
+    if os.path.exists(BASELINE_FILE):
+        os.remove(BASELINE_FILE)
+
+    files_processed = 0
+    with open(BASELINE_FILE, 'w') as file:
+        for root, dirs, files in os.walk(MONITOR_DIR):
+            for file in files:
+                filepath = os.path.join(root, file)
+                file_hash = calculate_hash(filepath)
+
+                if file_hash:
+                    file.write(f"{filepath}|{file_hash}\n")
+                    files_processed += 1
+                    print(f"Hashed: {filepath}")
+
+    print(f"Baseline created with {files_processed} file.")
 
 
 def main():
