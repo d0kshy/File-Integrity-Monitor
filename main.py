a64254f5b1ab25f1f6b7a5a49ee36fa5f115503f
d0kshy <dariakurnosova.contact@gmail.com>
2026-02-08 09:58:55 +0100

    Created a function that finds a hash of file

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import hashlib
 import os
+import time
+
+
+def calculate_hash(filepath):
+    sha256_hash = hashlib.sha256
+
+    try:
+        with open(filepath, 'rb') as file:
+            while chunk := file.read(8192):
+                sha256_hash.update(chunk)
+
+        return sha256_hash.hexdigest()
+
+    except FileNotFoundError:
+        return None
+    except PermissionError:
+        return None
 
 
 def main():
@@ -10,11 +27,11 @@ def main():
     choice = input("Select Option (A/B): ").upper()
 
     # if choice == 'A':
-    # Add a function that creates a baseline.txt
+    # Add a function to create baseline.txt
     # elif choice == 'B':
-    # Add a function that starts monitoring
+    # Add a function to start monitoring
     # else:
-    # print("Invalid choice.")
+    #    print("Invalid choice.")
 
 
 if __name__ == "__main__":
